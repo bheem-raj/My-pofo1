9fdf65af027b8d18871c7154bf09548f88b7e754
bheem-raj <psbheemaraj93@gmail.com>
2019-02-28 20:37:55 +0530

    mongodb connect

--- routers/admin.js
+++ routers/admin.js
@@ -1,6 +1,22 @@
 let data = require('../my-data.json')
 let express = require('express');
 let router = express.Router();
+let Client = require('mongodb').MongoClient;
+
+
+let dbUrl = 'mongodb://localhost:27017';
+
+
+let db;
+
+Client.connect(dbUrl, { useNewUrlParser: true }, function (error, client) {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log('Successfully Connected to DB');
+        db = client.db('mean')
+    }
+})
 
 router.get('/dashboard',(req,res)=>{
     res.render('admin/dashboard',{
@@ -52,18 +68,41 @@ router.get('/projects', (req, res) => {
 //     }
     
 // })
-    
-// router.get('/projects/:alias',(req,res)=> {
-//     let alias = req.params.alias;
+router.get('/projects/create', (req, res) => {
+    res.render('admin/project-create', {
+        title: "Create New Project",
+        layout: "examples"
 
-//     let index = data.projectIndex[alias];
-//     let project = data.myProjects[index];
+    })
 
-//     res.render('admin/project-detail',{
-//         title:'Project List',
-//         layout:'examples',
-//         project: project
-//     })
-// }) 
+})
+router.post('/projects/create', (req, res) => {
+    let data = req.body;
+    console.log(data)
+    let projectCollection = db.collection('projects');
+    projectCollection.insertOne(data, function (err, project) {
+        if (err) {
+            console.log(err)
+            next(err)
+        } else {
+            console.log(project.toJSON())
+            res.redirect('/admin/projects')
+        }
+    })
+})
+
+
+router.get('/projects/:alias',(req,res)=> {
+    let alias = req.params.alias;
+
+    let index = data.projectIndex[alias];
+    let project = data.myProjects[index];
+
+    res.render('admin/project-detail',{
+        title:'Project List',
+        layout:'examples',
+        project: project
+    })
+}) 
 
 module.exports =  router;
